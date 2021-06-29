e2cb0f7a489886edb2184bf6fb0709a7fd9e5cc9
Michelle Li <limichelle009@gmail.com>
2021-06-29 12:33:05 -0700

    Initial Commit

---
+++ mindful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mindful
+//
+//  Created by  Scholar on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
