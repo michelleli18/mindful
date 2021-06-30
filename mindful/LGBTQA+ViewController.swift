8357cb02d120897ddcb0321033184668b90ea06a
Michelle Li <limichelle009@gmail.com>
2021-06-29 18:32:27 -0700

    Added new section pages

---
+++ mindful/LGBTQA+ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LGBTQA+ViewController.swift
+//  mindful
+//
+//  Created by  Scholar on 6/29/21.
+//
+
+import UIKit
+
+class LGBTQA_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func homeButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
