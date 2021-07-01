c03c9401c3d9c6a95fc8761e954a703a6838d3cb
Nidhi Pabbathi <tigernidhi@hotmail.com>
2021-07-01 11:44:01 -0500

    added constraints

--- mindful/HomePageViewController.swift
+++ mindful/HomePageViewController.swift
@@ -25,14 +25,17 @@ class HomePageViewController: UIViewController {
         studentPressuresButton.layer.cornerRadius = 12
         studentPressuresButton.layer.borderWidth = 5
         studentPressuresButton.layer.borderColor = UIColor(red: 0, green: 61/255, blue: 57/255, alpha: 1).cgColor
+        studentPressuresButton.titleLabel?.textAlignment = .center
         
         blmsahButton.layer.cornerRadius = 12
         blmsahButton.layer.borderWidth = 5
         blmsahButton.layer.borderColor = UIColor(red: 0, green: 61/255, blue: 57/255, alpha: 1).cgColor
+        blmsahButton.titleLabel?.textAlignment = .center
         
         covidButton.layer.cornerRadius = 12
         covidButton.layer.borderWidth = 5
         covidButton.layer.borderColor = UIColor(red: 0, green: 61/255, blue: 57/255, alpha: 1).cgColor
+        covidButton.titleLabel?.textAlignment = .center
     }
     
 
